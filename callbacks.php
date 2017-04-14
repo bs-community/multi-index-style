3d00c2930cb3220429345e517ee964f2f866960f
gplane <g-plane@hotmail.com>
2017-04-15 00:25:50 +0800

    Remove old view files

--- callbacks.php
+++ callbacks.php
@@ -3,5 +3,22 @@
 return [
     App\Events\PluginWasEnabled::class => function () {
         Artisan::call('view:clear');
+    },
+    'PluginWasInstalled' => function () {
+        if (file_exists($path = base_path('plugins/multi-index-style/views/index/index-default.tpl'))) {
+            unlink($path);
+        }
+        if (file_exists($path = base_path('plugins/multi-index-style/views/index/index-fixed.tpl'))) {
+            unlink($path);
+        }
+        if (file_exists($path = base_path('plugins/multi-index-style/views/index/index-old.tpl'))) {
+            unlink($path);
+        }
+        if (file_exists($path = base_path('plugins/multi-index-style/views/config.tpl'))) {
+            unlink($path);
+        }
+        if (file_exists($path = base_path('plugins/multi-index-style/views/example.tpl'))) {
+            unlink($path);
+        }
     }
 ];
