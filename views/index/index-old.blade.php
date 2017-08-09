f5dae9671772079f08b7c4bed3a824b2d1f7f776
gplane <g-plane@hotmail.com>
2017-08-09 15:34:49 +0800

    Add extra links in user-menu for admins

--- views/index/index-old.blade.php
+++ views/index/index-old.blade.php
@@ -58,31 +58,35 @@
                             @include('vendor.language')
                             @if (!is_null($user))
                             <!-- User Account Menu -->
-                            <li class="dropdown user user-menu">
-                                <!-- Menu Toggle Button -->
-                                <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-                                    <!-- The user image in the navbar-->
-                                    <img src="{{ avatar($user, 25) }}" class="user-image" alt="User Image">
-                                    <!-- hidden-xs hides the username on small devices so only the image appears. -->
-                                    <span class="hidden-xs nickname">{{ bs_nickname($user) }}</span>
-                                </a>
-                                <ul class="dropdown-menu">
-                                    <!-- The user image in the menu -->
-                                    <li class="user-header">
-                                        <img src="{{ avatar($user, 128) }}" alt="User Image">
-                                        <p>{{ $user->email }}</p>
-                                    </li>
-                                    <!-- Menu Footer-->
-                                    <li class="user-footer">
-                                        <div class="pull-left">
-                                            <a href="{{ url('user') }}" class="btn btn-default btn-flat">{{ trans('general.user-center') }}</a>
-                                        </div>
-                                        <div class="pull-right">
-                                            <a href="javascript:logout();" class="btn btn-default btn-flat">{{ trans('general.logout') }}</a>
-                                        </div>
-                                    </li>
-                                </ul>
-                            </li>
+                                @if (view()->exists('common.user-menu'))
+                                    @include('common.user-menu')
+                                @else
+                                <li class="dropdown user user-menu">
+                                    <!-- Menu Toggle Button -->
+                                    <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+                                        <!-- The user image in the navbar-->
+                                        <img src="{{ avatar($user, 25) }}" class="user-image" alt="User Image">
+                                        <!-- hidden-xs hides the username on small devices so only the image appears. -->
+                                        <span class="hidden-xs nickname">{{ bs_nickname($user) }}</span>
+                                    </a>
+                                    <ul class="dropdown-menu">
+                                        <!-- The user image in the menu -->
+                                        <li class="user-header">
+                                            <img src="{{ avatar($user, 128) }}" alt="User Image">
+                                            <p>{{ $user->email }}</p>
+                                        </li>
+                                        <!-- Menu Footer-->
+                                        <li class="user-footer">
+                                            <div class="pull-left">
+                                                <a href="{{ url('user') }}" class="btn btn-default btn-flat">{{ trans('general.user-center') }}</a>
+                                            </div>
+                                            <div class="pull-right">
+                                                <a href="javascript:logout();" class="btn btn-default btn-flat">{{ trans('general.logout') }}</a>
+                                            </div>
+                                        </li>
+                                    </ul>
+                                </li>
+                                @endif
                             @else {{-- Anonymous User --}}
                             <!-- User Account Menu -->
                             <li class="dropdown user user-menu">
