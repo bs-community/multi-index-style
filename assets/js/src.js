7e90f7b1a5c8b4607ecdf7ca3f27f8c8a69b5738
Pig Fang <g-plane@hotmail.com>
2018-03-02 16:45:49 +0800

    style: use ESLint

--- assets/js/src.js
+++ assets/js/src.js
@@ -1,27 +1,28 @@
 function getAppendContent (item) {
   switch (item.type) {
-    case 'link_new_tab':
-      return (
-        '<li><a href="' +
-        item.link +
-        '" target="_blank">' +
-        item.text +
-        '</a></li>'
-      )
-      break
-    case 'link':
-      return '<li><a href="' + item.link + '">' + item.text + '</a></li>'
-      break
+  case 'link_new_tab':
+    return (
+      `<li><a href="${
+        item.link
+      }" target="_blank">${
+        item.text
+      }</a></li>`
+    )
+  case 'link':
+    return `<li><a href="${item.link}">${item.text}</a></li>`
+  default:
+    return ''
   }
 }
 
 function addNavBarItems (list) {
   for (const item of list) {
-    if (item.type == 'dropdown') {
-      let content =
-        '<li><a href="#" class="dropdown-toggle" data-toggle="dropdown"><span>' +
-        item.title +
-        '</span><span class="caret"></span></a><ul class="dropdown-menu" role="menu">'
+    if (item.type === 'dropdown') {
+      let content
+        = `<li>
+        <a href="#" class="dropdown-toggle" data-toggle="dropdown"><span>${
+  item.title
+}</span><span class="caret"></span></a><ul class="dropdown-menu" role="menu">`
       for (let j = 0; j < item.count; j++) {
         content += getAppendContent(item.menu[j])
       }
@@ -36,13 +37,16 @@ function addNavBarItems (list) {
 function addFeatureText (data) {
   $('#feature-1-title').text(data.feature_1_title)
   $('#feature-1-text').text(data.feature_1_text)
-  $('.fa-users').removeClass('fa-users').addClass(data.feature_1_fa)
+  $('.fa-users').removeClass('fa-users')
+    .addClass(data.feature_1_fa)
   $('#feature-2-title').text(data.feature_2_title)
   $('#feature-2-text').text(data.feature_2_text)
-  $('.fa-share-alt').removeClass('fa-share-alt').addClass(data.feature_2_fa)
+  $('.fa-share-alt').removeClass('fa-share-alt')
+    .addClass(data.feature_2_fa)
   $('#feature-3-title').text(data.feature_3_title)
   $('#feature-3-text').text(data.feature_3_text)
-  $('.fa-cloud').removeClass('fa-cloud').addClass(data.feature_3_fa)
+  $('.fa-cloud').removeClass('fa-cloud')
+    .addClass(data.feature_3_fa)
   $('#footer-introduction').text(data.introdution_text)
   $('#btn-register').text(data.start_button_text)
 }
